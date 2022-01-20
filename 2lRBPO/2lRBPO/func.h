b2b1af5314d7e94ac9b3224521d882f99dfdfcb1
AlinaPash <90476708+AlinaPash@users.noreply.github.com>
2022-01-20 18:15:57 +0300

    Delete 2lRBPO/2lRBPO directory

--- 2lRBPO/2lRBPO/func.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <math.h>
-extern double x;
-extern double result;
-static void f()
-{
-	result = (5-2*x*x)/((1+x+x*x)/(2*x+x*x)+2-(1-x-x*x)/(2*x-x*x));
-}
